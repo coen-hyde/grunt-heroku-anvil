ee5067037b24ee142c30dc726170a62ce57d4826
Coen Hyde <coen.hyde@gmail.com>
2013-09-10 16:33:45 +1000

    initial commit

---
+++ tasks/heroku_anvil.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-heroku-anvil
+ * https://github.com/coen-hyde/grunt-heroku-anvil
+ *
+ * Copyright (c) 2013 Coen Hyde
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('heroku_anvil', 'Uses Heroku's Anvil build server. Then deploy optionally deploys the build to a specified app on Heroku. This is an alternative to the standard git deployment.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
